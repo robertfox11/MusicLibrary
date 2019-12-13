01e231e569a4eec4ed66f338394d27eb1e6f7cb1
robert Salazar <11rsahome@gmail.com>
2019-12-13 12:59:10 +0100

    add class songs and artist [class]

---
+++ assets/js/album.js
@@ -0,0 +1,11 @@
+export class Album {
+    constructor(caratula, nameAlbum, nameArtist, priceAlbum, numSongs, releaseDate, generMusic) {
+        this.caratula = caratula;
+        this.nameAlbum = nameAlbum;
+        this.nameArtist = nameArtist;
+        this.priceAlbum = priceAlbum;
+        this.numSongs = numSongs;
+        this.releaseDate = releaseDate;
+        this.generMusic = generMusic;
+    }
+}
---
+++ assets/js/video.js
@@ -0,0 +1,13 @@
+export class Video {
+    constructor(cover, nameSongs, artistName, songsPrice, releaseDate, songLength, musicGner, clipVideo, linkMusiVideo) {
+        this.cover = cover;
+        this.nameSongs = nameSongs;
+        this.artistName = artistName;
+        this.songsPrice = songsPrice;
+        this.releaseDate = releaseDate;
+        this.songLength = songLength;
+        this.musicGner = musicGner;
+        this.clipVideo = clipVideo;
+        this.linkMusiVideo = linkMusiVideo;
+    }
+}
