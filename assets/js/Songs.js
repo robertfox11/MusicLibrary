41fb99397f1f22edc247099562c38333f9217d53
robert Salazar <11rsahome@gmail.com>
2019-12-14 16:10:30 +0100

    function getsonname[class]

--- assets/js/Songs.js
+++ assets/js/Songs.js
@@ -1,4 +1,4 @@
-export class Songs {
+class Songs {
     constructor(caratula, songsNames, artistNames, albumNames, songsPrices, releaseDate, songLength, musicGener, audioSong, linkSongs) {
         this.caratula = caratula;
         this.songsNames = songsNames;
@@ -12,4 +12,33 @@ export class Songs {
         this.linkSongs = linkSongs;
     }
 
+    getSongNAmes() {
+        var tags = $('#autocomplete');
+        let cors = 'https://cors-anywhere.herokuapp.com';
+        // console.log(tags.value)
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: "https://itunes.apple.com/search?term=" + tags.val(),
+            // + this.songsNames + "&limit=5",
+            success: function(response) {
+                console.log(response)
+
+                var listaUsuarios = $("#lista-usuarios");
+
+                $.each(response.results, function(index, element) {
+                    listaUsuarios.append(
+                        '<div>' +
+                        '<p>' + element.artistName + ' ' + element.trackName + '</p>' +
+                        '<img src=' + element.artworkUrl30 + '></img>' +
+                        '</div>'
+                    );
+                });
+            },
+            error: function() {
+                console.log("No se ha podido obtener la información");
+            },
+            timeout: 3000
+        });
+    }
 }
--- assets/js/prueba.js
+++ assets/js/prueba.js
@@ -1,13 +1,48 @@
-// import { getDefaultSettings } from "http2"
+// import { Songs } from "./Songs";
+$(document).ready(function() {
+    var songs = new Songs();
+    // songs.getSongsNames()
 
-$('#ajax').click(function() {
-    var _load = $('#load');
-    _load.load("https://itunes.apple.com/search?term=jack+johnson&limit=25");
-    console.log(_load)
-        // getDatos();
+    // var user = [];
+    $('#ajax').click(function() {
+
+    });
+    $("#boton-usuarios").on("click", function() {
+        songs.getSongNAmes()
+            // let cors = 'https://cors-anywhere.herokuapp.com';
+    });
+    // $('#autocomplete').autocomplete({
+    //     serviceUrl: '/autocomplete/countries',
+    //     onSelect: function(suggestion) {
+    //         alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+    //     }
+    // });
+    // var countries = [
+    //     { value: 'Andorra', data: 'AD' },
+    //     // ...
+    //     { value: 'Zimbabwe', data: 'ZZ' }
+    // ];
+    // $("#autocomplete").autocomplete({
+    //     source: countries
+
+    // });
+    // $('#autocomplete').autocomplete({
+    //     source: countries,
+    //     onSelect: function(suggestion) {
+    //         alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+    //     }
+    // });
+    // var _formInput = $('input');
+    // _formInput.on('keydown', function(params) {
+    //     var _text = $("#text");
+    //     // _text.html(params.type + ":" + params.which);
+    //     _text.text($(this).val()).show();
+
 });
+
 
 // function getDatos() {
 //     let url = 'https://itunes.apple.com/search? término = jack + johnson';
 //     console.log(url)
 // }
+// })
