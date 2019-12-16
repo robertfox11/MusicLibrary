613fd942fcd24930a7db2897593f523552821883
robert Salazar <11rsahome@gmail.com>
2019-12-16 17:30:49 +0100

    add  [Class Songs ]

--- assets/js/Artist.js
+++ assets/js/Artist.js
@@ -1,7 +1,85 @@
 export class Artist {
-    constructor(nameArtis, musicGener, linkGener) {
+    constructor(id, nameArtis, musicGener, linkGener) {
+        this.id = id;
         this.nameArtis = nameArtis;
         this.musicGener = musicGener;
         this.linkGener = linkGener;
+    }
+    getArtis() {
+        var tags = $('#autocomplete');
+        console.log(tags.val())
+        let cors = 'https://cors-anywhere.herokuapp.com';
+        let link = 'https://www.apple.com/itunes/link/';
+        // console.log(tags.value)
+        $.ajax({
+            type: 'GET',
+            dataType: 'jsonp',
+            url: "https://itunes.apple.com/search?term=" + tags.val(),
+            data: tags,
+            success: function(response) {
+                console.log(response)
+
+                var listaSearch = $("#list-search");
+
+                $.each(response.results, function(index, element) {
+                    var song = new Songs(element['trackId'], element['artistName'], element['primaryGenreName'], element['trackViewUrl'])
+                    listaSearch.append(
+                        '<div class="mb-4 card-desk col-md-4 text-center">' +
+                        '<div class="mb-1 card shadow-sm">' +
+                        '<div class="card-header">' +
+                        '<h2 class="my-0 font-weight-bold">' + song.artistNames + '</h2>' +
+                        '</div>' +
+                        '<div class="card-body">' +
+                        '<img src=' + song.caratula + ' class="imgSearch"></img>' +
+                        '<h4 class="mb-1">' + song.musicGener +
+                        '</h4>' +
+                        '<p>' + song.id + '</p>' +
+                        '<a  class="badge badge-primary" data-id=' + song.id + ' ' + 'href=' + song.linkSongs + ' target=_blank>link</a>' +
+                        '<audio id=' + 'play' + song.id + ' ' + 'src=' + song.audioSong + '></audio>' +
+                        '<div>' +
+                        '<button id=' + 'btnPlay' + song.id + ' ' + ' submit="submit" >play</button>' +
+                        '<button id=' + 'btnPause' + song.id + ' ' + 'submit="submit">pause</button>' +
+                        '<button id=' + 'btnStop' + song.id + ' ' + 'submit="submit">Stop</button>' +
+                        '</div> ' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
+                    );
+
+                    var btnPlay = document.querySelectorAll('#btnPlay' + song.id);
+                    console.log(btnPlay)
+                    var btnPause = document.querySelectorAll('#btnPause' + song.id);
+                    var btnStop = document.querySelectorAll('#btnStop' + song.id);
+                    var _audio = document.querySelectorAll('#play' + song.id);
+                    console.log(btnStop)
+
+                    btnPlay.forEach(function(index) {
+                        index.addEventListener('click', function() {
+                            _audio.forEach(function(params) {
+                                params.play();
+                            })
+                        })
+                    })
+                    btnPause.forEach(function(index) {
+                        index.addEventListener('click', function() {
+                            _audio.forEach(function(params) {
+                                params.pause();
+                            })
+                        })
+                    })
+                    btnStop.forEach(function(index) {
+                        index.addEventListener('click', function() {
+                            _audio.forEach(function(params) {
+                                params.load;
+                            })
+                        })
+                    })
+                });
+            },
+            error: function() {
+                console.log("No se ha podido obtener la información");
+            },
+            timeout: 3000
+        });
     }
 }
